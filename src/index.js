af526c233394a75b3f279a974481c0c753fd1429
endulum <apheum@zoho.com>
2023-03-05 21:29:35 -0800

    fetch and display minimal weather info in console

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
-console.log('owo');
+class Weather {
+    constructor(data) {
+        this.location = data.name;
+        this.weather = data.weather[0].description;
+        this.temperature = data.main.temp;
+    }
+
+    print() {
+        console.log(`${this.location}\n${this.weather}\n${this.temperature}`);
+    }
+}
+
+let currentWeather;
+
+const searchLocation = async term => {
+    const url = 'https://api.openweathermap.org/data/2.5/weather?q=';
+    const key = '&appid=6c0558fee84cc61371a98f44f922bcb2'
+    const response = await fetch(url + term + key, {mode: 'cors'});
+    const data = await response.json();
+    currentWeather = new Weather(data);
+    currentWeather.print();
+}
+
+searchLocation('Tokyo');
